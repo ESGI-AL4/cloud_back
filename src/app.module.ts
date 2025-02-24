2429efa0a96290d823d2857441b51db6644f2dde
Baguette42 <87232192+Baguette42@users.noreply.github.com>
2025-02-24 11:52:28 +0100

    add Room config for RDS BDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,13 @@ import { Card, Quizz } from './entities';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
-      entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     TypeOrmModule.forFeature([Card]),
