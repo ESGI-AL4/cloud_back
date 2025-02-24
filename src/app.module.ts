28785a62e90b1ccf29facdf9d4e35344cb7148f9
Baguette42 <87232192+Baguette42@users.noreply.github.com>
2025-02-24 12:24:11 +0100

    add mysql package

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { Card, Quizz } from './entities';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10) || 3306,
+      port: 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
